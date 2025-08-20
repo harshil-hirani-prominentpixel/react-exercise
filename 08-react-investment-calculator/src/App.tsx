e4a0a1a2c39b29bdbdf83f0aad8b38e6e116d1ab
harshil-hirani-prominentpixel <harshil.hirani@prominentpixel.com>
2025-08-20 16:09:55 +0530

    Modified : react-investment-calculator app

--- 08-react-investment-calculator/src/App.tsx
+++ 08-react-investment-calculator/src/App.tsx
@@ -128,13 +128,6 @@ function App() {
                 </tr>
               );
             })}
-            <tr>
-              <td>YEAR NUMBER</td>
-              <td>TOTAL SAVINGS END OF YEAR</td>
-              <td>INTEREST GAINED IN YEAR</td>
-              <td>TOTAL INTEREST GAINED</td>
-              <td>TOTAL INVESTED CAPITAL</td>
-            </tr>
           </tbody>
         </table>
       ) : (
